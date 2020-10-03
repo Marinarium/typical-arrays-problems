8f8424429ab0c513a6e6052ffb3c1f8072c45c93
Marinarium <schwmk@gmail.com>
2020-10-03 17:07:11 +0300

    feat: max min avg for arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array !== undefined && array.length > 0) {
+    var minElem = Math.min.apply(null, array);
+    return minElem;
+  } else {
+    return 0;
+  }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array !== undefined && (array.length > 0) ) {
+    var maxElem = Math.max.apply(null, array);
+    return maxElem;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array !== undefined && (array.length > 0) ) {
+    return array.reduce((a, b) => (a + b)) / array.length;
+  } else {
+    return 0;
+  }
 }
